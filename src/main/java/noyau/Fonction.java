ac6d874c108a16c3bfbd0446ba29b2fe27a0e6fc
Santiago Bragagnolo <santiago.bragagnolo@univ-lille.fr>
2021-01-12 14:46:04 +0100

    Update Fonction.java

--- src/main/java/noyau/Fonction.java
+++ src/main/java/noyau/Fonction.java
@@ -3,5 +3,8 @@ package noyau;
 import java.util.*; 
 
 public abstract class Fonction extends Formule{
+    /**TODO: le type set ne garantis pas l'ordre des cases. Pour la somme n'est pas un probleme, de qu'on veut implementer des fonctions non commutatives on aurais des problemes ici.
+    Aussi, le set ne laisse pas la repetition. De qu'on veut utiliser la meme case pour plusieurs parties d'une fonction, on ne peut pas non plus. 
+    */
     public Set<Case> setCase = new HashSet<Case>();
 }
