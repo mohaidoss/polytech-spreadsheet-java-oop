d73fd3ff00f682945129ca6a66cf34737f02eb8e
Santiago Bragagnolo <santiago.bragagnolo@univ-lille.fr>
2021-01-12 14:59:37 +0100

    Update Moyenne.java

--- src/main/java/noyau/Moyenne.java
+++ src/main/java/noyau/Moyenne.java
@@ -5,6 +5,7 @@ import java.util.*;
 public class Moyenne extends Fonction {
     @Override
     public double eval(){
+        /** TODO: Commentest-ce que tu  pense gerer le case de formule sans cases?= 0/0 */
         return (super.setCase.stream().mapToDouble(m->m.valeur()).sum())/(super.setCase.size());
     }
 }
